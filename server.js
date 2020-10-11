9f2a2dd6232ab50306c0e0a31e5cc6149c9b7406
unknown <haoguan200@gmail.com>
2020-10-11 15:48:59 -0700

    delete added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// requires express, path, file system (fs) and uuid for generating unique id
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -9,28 +10,33 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// gets you the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// gets you the objects in json db
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+// gets you the notes in json db
 app.get("/api/notes/:id", function(req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
 
+// gets you index.html when type in random address
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// post input into json db
 app.post('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json')));
     const note = req.body;
 
-    // push note to notes
+
     notes.push({
         id: uuid4(),
         title: note.title,
@@ -41,10 +47,25 @@ app.post('/api/notes', (req, res) => {
     res.json(notes); 
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    res.json()
-});
+
+//deletes object from json db
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+   
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = uuid4();
+        
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
 
 const port = process.env.PORT || 3000;
 
